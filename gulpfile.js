3576bda5bc16931c3df9415bd78ae407e46affd4
Alexandr <chernousov013@gmail.com>
2023-02-09 13:12:46 +0700

    first com first com

--- gulpfile.js
+++ gulpfile.js
@@ -19,13 +19,8 @@ gulp.task('server', function() {
 gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({
-            suffix: '.min',
-            prefix: '',
-        }))
-        .pipe(autoprefixer({
-            cascade: false
-        }))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
